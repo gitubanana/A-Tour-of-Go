f2a8de23c14b44e8e108bb03c3533c887de42497
gitubanana <ptyolo666@gmail.com>
2024-09-08 18:13:18 +0900

    Feat: zero values (basics/12)

---
+++ basics/ZeroValues/zero.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	// %v : value in a default format
+	// %q : a single-quoted character literal
+}
