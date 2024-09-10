d2795f4c626855e02c8a5ee01dbe2d3249bce415
gitubanana <ptyolo666@gmail.com>
2024-09-10 18:38:55 +0900

    Feat: if with a short statement (flowcontrol/6)

---
+++ flowcontrol/IfWithAShortStatement/if-with-a-short-statement.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim { // return값에서 또 계산하지 않게 저장
+		return v
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
