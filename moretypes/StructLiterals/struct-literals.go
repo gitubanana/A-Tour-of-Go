6b52ba38dd7889b8857f1fa71419edacf4cc3ae3
gitubanana <ptyolo666@gmail.com>
2024-09-12 20:54:12 +0900

    Feat: Struct Literals (moretypes/5)

---
+++ moretypes/StructLiterals/struct-literals.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y int
+}
+
+var (
+	v1 = Vertex{1, 2} // Vertex 타입
+	v2 = Vertex{X: 1} // Y:0은 생략됨
+	v3 = Vertex{} // X:0과 Y:0 생략됨
+	p = &Vertex{1, 2} // *Vertex 타입
+)
+
+func main() {
+	fmt.Println(v1, p, v2, v3)
+}
