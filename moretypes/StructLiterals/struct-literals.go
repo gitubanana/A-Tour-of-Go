9667c7984d146c0c04caf7007f85700040acb7b8
gitubanana <ptyolo666@gmail.com>
2024-09-12 20:59:14 +0900

    Format: struct-literals.go

--- moretypes/StructLiterals/struct-literals.go
+++ moretypes/StructLiterals/struct-literals.go
@@ -7,10 +7,10 @@ type Vertex struct {
 }
 
 var (
-	v1 = Vertex{1, 2} // Vertex 타입
-	v2 = Vertex{X: 1} // Y:0은 생략됨
-	v3 = Vertex{} // X:0과 Y:0 생략됨
-	p = &Vertex{1, 2} // *Vertex 타입
+	v1 = Vertex{1, 2}  // Vertex 타입
+	v2 = Vertex{X: 1}  // Y:0은 생략됨
+	v3 = Vertex{}      // X:0과 Y:0 생략됨
+	p  = &Vertex{1, 2} // *Vertex 타입
 )
 
 func main() {
